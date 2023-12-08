bf1b83ad924028a08a8c84121eb294841de8bed4
Uwe Fechner <u.fechner-1@tudelft.nl>
2023-12-08 21:20:36 +0100

    Add tether_03.jl

---
+++ src/Tether_03.jl
@@ -0,0 +1,42 @@
+# Example three: Falling mass, attached to non-linear spring, no compression stiffnes
+using ModelingToolkit, OrdinaryDiffEq, PyPlot
+
+G_EARTH  = Float64[0.0, 0.0, -9.81]    # gravitational acceleration
+
+# model
+@param mass=1.0 c_spring=50.0 damping=0.5 l0=10.0
+@variables t pos(t)[1:3]=[0.0, 0.0,  0.0]
+@variables   vel(t)[1:3]=[0.0, 0.0, 50.0] 
+@variables   acc(t)[1:3]=[0.0, 0.0, -9.81] 
+D = Differential(t)
+
+eqs = vcat(D.(pos) ~ vel,
+           D.(vel) ~ acc,
+           acc    .~ G_EARTH)
+
+@named sys = ODESystem(eqs, t)
+simple_sys = structural_simplify(sys)
+
+duration = 10.0
+dt = 0.02
+tol = 1e-6
+tspan = (0.0, duration)
+ts    = 0:dt:duration
+u0 = zeros(6)
+u0[6] = 50.0
+
+prob = ODEProblem(simple_sys, u0, tspan)
+@time sol = solve(prob, Rodas5(), dt=dt, abstol=tol, reltol=tol, saveat=ts)
+
+X = sol.t
+POS_Z = sol(X, idxs=pos[3])
+VEL_Z = sol(X, idxs=vel[3])
+
+plot(X, POS_Z, color="green")
+xlabel("time [s]")
+grid(true)
+twinx()
+ylabel("vel_z [m/s]") 
+plot(X, VEL_Z, color="red") 
+PyPlot.show(block=false)
+nothing
