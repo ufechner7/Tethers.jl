81c316f98a6a7c3f3f6725bb697e53e0f67ae4c0
Uwe Fechner <fechner@aenarete.eu>
2023-12-11 16:26:46 +0100

    First steps

---
+++ src/Tether_07.jl
@@ -0,0 +1,49 @@
+"""
+Tutorial example simulating a 3D mass-spring system with a nonlinear spring (no spring forces
+for l < l_0) and reel-out. It uses five tether segments. Added parameter NONLINEAR: If it is set to `false`, 
+than a linear springs are used.
+Damping now increases with the segment length. 
+"""
+
+using ModelingToolkit, OrdinaryDiffEq, PyPlot, LinearAlgebra
+
+G_EARTH     = Float64[0.0, 0.0, -9.81]          # gravitational acceleration     [m/s²]
+L0::Float64 = -10.0                             # initial segment length            [m]
+V0::Float64 = 4                                 # initial velocity of lowest mass [m/s]
+segments::Int64 = 5
+POS0 = zeros(3, segments+1)
+for i in 1:segments+1
+    POS0[:, i] .= [0.0, 0, (i-1)*L0]
+end
+VEL0 = zeros(3, segments+1)
+for i in 1:segments+1
+    VEL0[:, i] .= [0.0, 0, (i-1)*V0/segments]
+end
+ACC0 = zeros(3, segments+1)
+for i in 2:segments+1
+    ACC0[:, i] .= G_EARTH
+end
+
+# model, Z component upwards
+@parameters mass=1.0 c_spring0=50.0 damping=0.5 l_seg=L0
+@variables t 
+@variables pos(t)[1:3, 1:segments]  = POS0
+@variables vel(t)[1:3, 1:segments]  = VEL0
+@variables acc(t)[1:3, 1:segments]  = ACC0
+# @variables unit_vector(t)[1:3]  = [0.0, 0.0, -sign(L0)]
+# @variables c_spring(t) = c_spring0
+# @variables spring_force(t)[1:3] = [0.0, 0.0, 0.0]
+# @variables force(t) = 0.0 norm1(t) = abs(l0) spring_vel(t) = 0.0
+# D = Differential(t)
+
+# eqs = vcat(D.(pos)      ~ vel,
+#            D.(vel)      ~ acc,
+#            norm1        ~ norm(pos),
+#            unit_vector  ~ -pos/norm1,         # direction from point mass to origin
+#            spring_vel   ~ -unit_vector ⋅ vel,
+#            c_spring     ~ c_spring0 * (norm1 > abs(l0)),
+#            spring_force ~ (c_spring * (norm1 - abs(l0)) + damping * spring_vel) * unit_vector,
+#            acc          ~ G_EARTH + spring_force/mass)
+
+# @named sys = ODESystem(eqs, t)
+# simple_sys = structural_simplify(sys)
