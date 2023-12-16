5b6beac2f69ec504587b07bb967be93ebc294d45
Uwe Fechner <fechner@aenarete.eu>
2023-12-16 12:31:39 +0100

    Improve sys image

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,11 +1,11 @@
 @info "Loading packages ..."
-using ModelingToolkit, OrdinaryDiffEq, PackageCompiler, PyPlot
+using ModelingToolkit, OrdinaryDiffEq, PackageCompiler, PyPlot, Timers
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:ModelingToolkit, :OrdinaryDiffEq, :PyPlot];
+    [:ModelingToolkit, :OrdinaryDiffEq, :Timers];
     sysimage_path="kps-image_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
