50eb1c09be90982cfd67295519a63052ecddd3c1
Uwe Fechner <u.fechner-1@tudelft.nl>
2023-12-16 23:43:54 +0100

    Update MWE01

--- mwes/mwe01.jl
+++ mwes/mwe01.jl
@@ -3,7 +3,9 @@ using ModelingToolkit, LinearAlgebra
 @variables t pos(t)[1:3] = [0.0, 0.0, 10.0]
 
 # working
-pos/norm(pos)
+# pos/norm(pos)
 
 # not working
-# normalize(pos)
+@register_symbolic LinearAlgebra.normalize(pos)
+
+normalize(pos)
