39a16487077b119e308b5541d548a1df32f9a3df
Uwe Fechner <fechner@aenarete.eu>
2023-12-16 14:38:00 +0100

    cleanup

--- src/init.jl
+++ src/init.jl
@@ -1,8 +1,10 @@
 println("Running init...")
 
 import PyPlot: plot as pl
+import PyPlot: xlabel, ylabel, grid, twinx, legend, figure
 
 function plot(x...; y...)
-    pl(x...; y...)
+    res = pl(x...; y...)
     PyPlot.show(block=false)
+    res
 end
