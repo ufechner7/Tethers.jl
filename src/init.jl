2488ef11799969aaa595b1d2b351653a0543265f
Uwe Fechner <fechner@aenarete.eu>
2023-12-16 16:16:29 +0100

    Make grid work

--- src/init.jl
+++ src/init.jl
@@ -1,10 +1,17 @@
 println("Running init...")
 
 import PyPlot: plot as pl
-import PyPlot: xlabel, ylabel, xlim, ylim, grid, twinx, legend, figure, scatter, annotate, gcf
+import PyPlot: xlabel, ylabel, xlim, ylim, twinx, legend, figure, scatter, annotate, gcf
 
 function plot(x...; y...)
     res = pl(x...; y...)
+    PyPlot.show(block=false)
+    res
+end
+
+function grid(x...)
+    res = PyPlot.grid(x)
+    PyPlot.pause(0.01)
     PyPlot.show(block=false)
     res
 end
