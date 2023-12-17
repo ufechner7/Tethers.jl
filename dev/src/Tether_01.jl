4560b35d2b613dc4a10845db71fad519e5043932
Documenter.jl <documenter@juliadocs.github.io>
2023-12-17 07:47:39 +0000

    build based on 0c45e62

--- dev/src/Tether_01.jl
+++ dev/src/Tether_01.jl
@@ -3,7 +3,7 @@ using ModelingToolkit, OrdinaryDiffEq
 
 G_EARTH  = Float64[0.0, 0.0, -9.81]    # gravitational acceleration
 
-# model
+# definition of the model
 @variables t pos(t)[1:3]=[0.0, 0.0,  0.0]
 @variables   vel(t)[1:3]=[0.0, 0.0, 50.0] 
 @variables   acc(t)[1:3]=[0.0, 0.0, -9.81] 
@@ -16,18 +16,17 @@ eqs = vcat(D.(pos) ~ vel,
 @named sys = ODESystem(eqs, t)
 simple_sys = structural_simplify(sys)
 
+# running the simulation
 duration = 10.0
 dt = 0.02
 tol = 1e-6
 tspan = (0.0, duration)
 ts    = 0:dt:duration
 
-# initial state
-u0 = Dict(vel=>[0, 0, 50.0])
-
-prob = ODEProblem(simple_sys, u0, tspan)
+prob = ODEProblem(simple_sys, nothing, tspan)
 @time sol = solve(prob, Rodas5(), dt=dt, abstol=tol, reltol=tol, saveat=ts)
 
+# plotting the result
 X = sol.t
 POS_Z = stack(sol[pos], dims=1)[:,3]
 VEL_Z = stack(sol[vel], dims=1)[:,3]
