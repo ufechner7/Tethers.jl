d7a9fd58c8addeb4db0dc7c7a4912792d7ac5686
Uwe Fechner <u.fechner-1@tudelft.nl>
2023-12-17 08:54:00 +0100

    Improve the examples

--- src/Tether_02.jl
+++ src/Tether_02.jl
@@ -4,7 +4,7 @@ using ModelingToolkit, OrdinaryDiffEq, LinearAlgebra
 G_EARTH  = Float64[0.0, 0.0, -9.81]    # gravitational acceleration [m/s²]
 L0::Float64 = -10.0                    # initial spring length      [m]
 
-# model, Z component upwards
+# defing the model, Z component upwards
 @parameters mass=1.0 c_spring=50.0 damping=0.5 l0=L0
 @variables t pos(t)[1:3] = [0.0, 0.0,  L0]
 @variables   vel(t)[1:3] = [0.0, 0.0,  0.0] 
@@ -25,28 +25,28 @@ eqs = vcat(D.(pos)      ~ vel,
 @named sys = ODESystem(eqs, t)
 simple_sys = structural_simplify(sys)
 
+# running the simulation
 duration = 10.0
 dt = 0.02
 tol = 1e-6
 tspan = (0.0, duration)
 ts    = 0:dt:duration
 
-# initial state
-u0 = Dict(pos=>[0,0,L0])
-
-prob = ODEProblem(simple_sys, u0, tspan)
+prob = ODEProblem(simple_sys, nothing, tspan)
 @time sol = solve(prob, Rodas5(), dt=dt, abstol=tol, reltol=tol, saveat=ts)
 
+# plotting the result
 X = sol.t
 POS_Z = stack(sol[pos], dims=1)[:,3]
 VEL_Z = stack(sol[vel], dims=1)[:,3]
 
 PyPlot.close()
-plot(X, POS_Z, color="green")
+line1, = plot(X, POS_Z, color="green", label="pos_z")
 xlabel("time [s]")
 ylabel("pos_z [m]")
 PyPlot.grid(true)
 twinx()
 ylabel("vel_z [m/s]") 
-plot(X, VEL_Z, color="red") 
+line2, = plot(X, VEL_Z, color="red", label="vel_z")
+legend(handles=[line1, line2]) 
 nothing
