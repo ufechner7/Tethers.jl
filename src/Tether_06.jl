732ec6f2aa95ec7719dd7f24b6e3fd7c52d23f67
Uwe Fechner <fechner@aenarete.eu>
2023-12-18 01:00:52 +0100

    Cleanup

--- src/Tether_06.jl
+++ src/Tether_06.jl
@@ -23,7 +23,6 @@ ACC0 = zeros(3, SEGMENTS+1)
 SEGMENTS0 = zeros(3, SEGMENTS) 
 UNIT_VECTORS0 = zeros(3, SEGMENTS)
 for i in 1:SEGMENTS+1
-    local l0
     l0 = -(i-1)*L0/SEGMENTS
     v0 = (i-1)*V0/SEGMENTS
     POS0[:, i] .= [sin(α0) * l0, 0, cos(α0) * l0]
