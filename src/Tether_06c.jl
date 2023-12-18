fcc7c82a998ddede85d1f8a4b58ae3901370099c
Uwe Fechner <u.fechner-1@tudelft.nl>
2023-12-18 12:46:04 +0100

    Minor improvement

--- src/Tether_06c.jl
+++ src/Tether_06c.jl
@@ -83,10 +83,14 @@ function model(se)
     eqs2 = vcat(eqs2, damping  ~ se.damping  / (length/se.segments))
     eqs = vcat(eqs1..., eqs2)
 
-    cb = [norm([pos[1, 1+1] - pos[1, 1], pos[2, 1+1] - pos[2, 1], pos[3, 1+1] - pos[3, 1]]) ~ abs(se.l0)]
-    for i in 2:se.segments
+    local cb
+    for i in 1:se.segments
         cbi = [norm([pos[1, i+1] - pos[1, i], pos[2, i+1] - pos[2, i], pos[3, i+1] - pos[3, i]]) ~ abs(se.l0)]
-        cb = vcat(cb, cbi)
+        if i == 1
+            cb = cbi
+        else
+            cb = vcat(cb, cbi)
+        end
     end
         
     @named sys = ODESystem(eqs, t; continuous_events = cb)
