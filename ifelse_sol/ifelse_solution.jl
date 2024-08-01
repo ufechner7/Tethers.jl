2ad4442c12ace98f9a20cc91b7aa769cf96867a7
1-Bart-1 <bart@vandelint.net>
2024-08-01 08:05:40 +0200

    succesful true false switch

--- ifelse_sol/ifelse_solution.jl
+++ ifelse_sol/ifelse_solution.jl
@@ -7,24 +7,26 @@ import IfElse
 vars = @variables y(t)
 pars = @parameters uMax uMin u
 @variables ifCond1(t) = false
+@variables ifCond2(t) = false
 D = Differential(t)
 
 continuous_events = [
-    (uMax - t ~ 0) => [ifCond1 ~ true]
+    (uMax - t ~ 0) => [ifCond1 ~ true, ifCond2 ~ false]
+    (uMin - t ~ 0) => [ifCond1 ~ false, ifCond2 ~ true]
 ]
 
 
 eqs = [
-  D(y) ~ IfElse.ifelse(ifCond1 == true, uMax, u),
+  D(y) ~ IfElse.ifelse(ifCond1 == true, uMax, IfElse.ifelse(ifCond2 == true, uMin, u)),
   D(ifCond1) ~ 0,
-  # D(ifCond2) ~ 0,
+  D(ifCond2) ~ 0,
 ]
 
-@named ifequationDer = ODESystem(eqs, t, vars, pars; continuous_events)
+@named ifequationDer = ODESystem(eqs, t; continuous_events)
 ifequationDer = structural_simplify(ifequationDer)
 tspan = (0.0,12.0)
 prob = ODEProblem(ifequationDer,
-                  [y => 0.0, ifCond1=>false],
+                  [y => 0.0, ifCond1=>false, ifCond2=>false],
                   tspan,
                   [uMax => 10.0,
                    uMin => 2.0,
