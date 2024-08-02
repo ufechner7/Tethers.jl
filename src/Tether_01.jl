f03242463fab622df54897e9636f2f0e3bc12060
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-08-02 04:23:35 +0200

    cleanup

--- src/Tether_01.jl
+++ src/Tether_01.jl
@@ -10,13 +10,9 @@ G_EARTH::Vector{Float64} = [0.0, 0.0, -9.81]    # gravitational acceleration    
 @variables   acc(t)[1:3]=[0.0, 0.0, -9.81] 
 D = Differential(t)
 
-pos = collect(pos)
-vel = collect(vel)
-acc = collect(acc)
-
-eqs = vcat(D.(pos) .~ vel,
-           D.(vel) .~ acc,
-           acc    .~ G_EARTH)
+eqs = vcat(D.(pos) ~ vel,
+           D.(vel) ~ acc,
+           acc     ~ G_EARTH)
 
 @named sys = ODESystem(eqs, t)
 simple_sys = structural_simplify(sys)
