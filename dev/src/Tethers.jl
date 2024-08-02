923edbbf1ca83d521ddc9e71972aee6423355fde
Documenter.jl <documenter@juliadocs.github.io>
2024-08-02 07:06:47 +0000

    build based on 2cebcc8

--- dev/src/Tethers.jl
+++ dev/src/Tethers.jl
@@ -2,12 +2,14 @@ module Tethers
 
 export docu
 
+LAUNCH_BROWSER = true
+
 function docu(build=true)
     @eval using LiveServer
     # if build
     #     include("docs/make.jl")
     # end
-    if Sys.islinux()
+    if Sys.islinux() && ! LAUNCH_BROWSER
         Base.run(`xdg-open "docs/build/index.html"`; wait=false)
     else
         Base.invokelatest(LiveServer.servedocs; skip_dir="docs", launch_browser=true)
