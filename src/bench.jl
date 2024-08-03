f704a866a8973988a7d2ad93c7207d484f6cec05
Uwe Fechner <fechner@aenarete.eu>
2024-08-03 13:52:01 +0200

    bench with and without drag

--- src/bench.jl
+++ src/bench.jl
@@ -1,9 +1,24 @@
-__BENCH__ = true; include("Tether_07.jl")
+__BENCH__ = true; 
+let
+    include("Tether_06c.jl")
+    set = deepcopy(Settings2())
+    set.duration = 10.0
+    set.callbacks = false
+    simple_sys, pos, vel = model(set)
+    sol, elapsed_time = simulate(set, simple_sys) # warm-up
+    sol, elapsed_time = simulate(set, simple_sys)
+    println("Tethers_06c, without callbacks")
+    println("Elapsed time: $(elapsed_time) s, speed: $(round(set.duration/elapsed_time)) times real-time\n")
+end
 
-set = deepcopy(Settings())
-set.duration = 10.0
-simple_sys, pos, vel = model(set)
-sol, elapsed_time = simulate(set, simple_sys) # warm-up
-sol, elapsed_time = simulate(set, simple_sys)
-
-println("Elapsed time: $(elapsed_time) s, speed: $(round(set.duration/elapsed_time)) times real-time")
+__BENCH__ = true; 
+let
+    include("Tether_07.jl")
+    set = deepcopy(Settings())
+    set.duration = 10.0
+    simple_sys, pos, vel = model(set)
+    sol, elapsed_time = simulate(set, simple_sys) # warm-up
+    sol, elapsed_time = simulate(set, simple_sys)
+    println("Tethers_07, with tether drag")
+    println("Elapsed time: $(elapsed_time) s, speed: $(round(set.duration/elapsed_time)) times real-time")
+end
