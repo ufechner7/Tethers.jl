841450ed45c63799cb4f6c8b173f55c5ebe74548
Uwe Fechner <fechner@aenarete.eu>
2024-08-04 23:37:33 +0200

    Creating .gif file works

--- src/Tether_06b.jl
+++ src/Tether_06b.jl
@@ -1,7 +1,9 @@
 # Tutorial example simulating a 3D mass-spring system with a nonlinear spring (no spring forces
-# for l < l_0), n tether segments and reel-in and reel-out. 
+# for l < l_0), n tether segments and reel-in and reel-out. Can create a video of the simulation.
+# For creating the video, set save=true in the Settings struct.
 using ModelingToolkit, OrdinaryDiffEq, LinearAlgebra, Timers, Parameters
 using ModelingToolkit: t_nounits as t, D_nounits as D
+using ControlPlots
 
 @with_kw mutable struct Settings @deftype Float64
     g_earth::Vector{Float64} = [0.0, 0.0, -9.81] # gravitational acceleration     [m/s²]
@@ -106,45 +108,33 @@ function simulate(se, simple_sys)
     sol
 end
 
-function plot2d(se, sol, pos, reltime, line, sc, txt, j)
-    index = Int64(round(reltime*50+1))
-    x, z = Float64[], Float64[]
-    for particle in 1:se.segments+1
-        push!(x, (sol(sol.t, idxs=pos[1, particle]))[index])
-        push!(z, (sol(sol.t, idxs=pos[3, particle]))[index])
-    end
-    z_max = maximum(z)
-    if isnothing(line)
-        line, = plot(x,z; linewidth="1")
-        sc  = scatter(x, z; s=15, color="red") 
-        txt = annotate("t=$(round(reltime,digits=1)) s",  
-                        xy=(se.l0/4.2, z_max-7), fontsize = 12)
-    else
-        line.set_xdata(x)
-        line.set_ydata(z)
-        sc.set_offsets(hcat(x,z))
-        txt.set_text("t=$(round(reltime,digits=1)) s")
-        gcf().canvas.draw()
+function play(se, sol, pos)
+    dt = 0.151
+    se.save = true # create video pics
+    ylim = (-1.2*(se.l0+se.v_ro*se.duration), 0.5)
+    xlim = (-se.l0/2, se.l0/2)
+    mkpath("video")
+    z_max = 0.0
+    # text position
+    xy = (se.l0/4.2, z_max-7)
+    start = time_ns()
+    i = 1
+    j = 0
+    for time in 0:dt:se.duration
+        # while we run the simulation in steps of 20ms, we update the plot only every 150ms
+        # therefore we have to skip some steps of the result
+        while sol.t[i] < time
+            i += 1
+        end
+        plot2d(sol[pos][i], time; segments=se.segments, xlim, ylim, xy)
+        if se.save
+            plt.savefig("video/"*"img-"*lpad(j,4,"0"))
+        end
+        j += 1
+        wait_until(start + 0.5*time*1e9)
     end
     if se.save
-        PyPlot.savefig("video/"*"img-"*lpad(j,4,"0"))
-    end
-    line, sc, txt
-end
-
-function play(se, sol, pos)
-    PyPlot.close()
-    dt = 0.151
-    ylim(-1.2*(se.l0+se.v_ro*se.duration), 0.5)
-    xlim(-se.l0/2, se.l0/2)
-    grid(true; color="grey", linestyle="dotted")
-    tight_layout(rect=(0, 0, 0.98, 0.98))
-    line, sc, txt = nothing, nothing, nothing
-    mkpath("video")
-    start = time_ns()
-    for (j, time) in pairs(0:dt:se.duration)
-        line, sc, txt = plot2d(se, sol, pos, time, line, sc, txt, j)
-        wait_until(start + 0.5*time*1e9)
+        include("export_gif.jl")
     end
     nothing
 end
--- src/export_gif.jl
+++ src/export_gif.jl
@@ -1,16 +1,15 @@
-# Before running this script you must install the packages that are used
-# with `using Pkg; Pkg.add("FileIO")` etc.
+using Pkg; Pkg.add("FileIO"); Pkg.add("ImageIO"); Pkg.add("Colors"); Pkg.add("FixedPointNumbers"); Pkg.add("Images")
 using FileIO, ImageIO, Colors, FixedPointNumbers, Images
 
 folder = "video"
 
 A = Array{Array{RGB{Normed{UInt8,8}},2},1}()
 
-for j in 0:146
+for j in 0:66
     global A
     local img
     img_path = folder*"/"*"img-"*lpad(j,4,"0")*".png"
-    img=load(img_path)
+    img=FileIO.load(img_path)
     if j==0
         A = img
     else
@@ -18,4 +17,6 @@ for j in 0:146
     end
 end
 
-save(folder * "/Tether.gif", A)
+FileIO.save(folder * "/Tether.gif", A)
+Pkg.rm("FileIO"); Pkg.rm("ImageIO"); Pkg.rm("Colors"); Pkg.rm("FixedPointNumbers"); Pkg.rm("Images")  
+println("\nGif file $(folder * "/Tether.gif") created!")  
