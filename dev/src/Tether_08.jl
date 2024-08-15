7ac54d06f5b3657bf042a72b0e91bd83e3e7ee80
Documenter.jl <documenter@juliadocs.github.io>
2024-08-15 15:53:09 +0000

    build based on a9b7b0b

--- dev/src/Tether_08.jl
+++ dev/src/Tether_08.jl
@@ -1,7 +1,8 @@
 # Tutorial example simulating a 3D mass-spring system with a nonlinear spring (no spring forces
 # for l < l_0), n tether segments and reel-in and reel-out. 
-using PyPlot
 using ModelingToolkit, OrdinaryDiffEq, LinearAlgebra, Timers, Parameters
+using ModelingToolkit: t_nounits as t, D_nounits as D
+using ControlPlots
 
 # TODO: Add aerodynamic drag
 
@@ -42,7 +43,6 @@ function model(se)
     POS0, VEL0, ACC0, SEGMENTS0, UNIT_VECTORS0 = calc_initial_state(se)
     mass_per_meter = se.rho_tether * pi * (se.d_tether/2000.0)^2    # rho * cross-section
     @parameters c_spring0=se.c_spring/(se.l0/se.segments) l_seg=se.l0/se.segments
-    @variables t 
     @variables pos(t)[1:3, 1:se.segments+1]  = POS0
     @variables vel(t)[1:3, 1:se.segments+1]  = VEL0
     @variables acc(t)[1:3, 1:se.segments+1]  = ACC0
@@ -58,10 +58,9 @@ function model(se)
     @variables c_spr(t)[1:se.segments] = c_spring0 * ones(se.segments)
     @variables spring_force(t)[1:3, 1:se.segments] = zeros(3, se.segments)
     @variables total_force(t)[1:3, 1:se.segments+1] = zeros(3, se.segments+1)
-    D = Differential(t)
 
-    eqs1 = vcat(D.(pos) ~ vel,
-                D.(vel) ~ acc)
+    eqs1 = vcat(D.(pos) .~ vel,
+                D.(vel) .~ acc)
     eqs2 = []
     for i in se.segments:-1:1
         eqs2 = vcat(eqs2, segment[:, i] ~ pos[:, i+1] - pos[:, i])
@@ -100,7 +99,7 @@ function model(se)
     eqs2 = vcat(eqs2, damping  ~ se.damping  / (length/se.segments))
     eqs = vcat(eqs1..., eqs2)
         
-    @named sys = ODESystem(eqs, t)
+    @named sys = ODESystem(Symbolics.scalarize.(reduce(vcat, Symbolics.scalarize.(eqs))), t)
     simple_sys = structural_simplify(sys)
     simple_sys, pos, vel
 end
@@ -115,45 +114,31 @@ function simulate(se, simple_sys)
     sol
 end
 
-function plot2d(se, sol, pos, reltime, line, sc, txt, j)
-    index = Int64(round(reltime*50+1))
-    x, z = Float64[], Float64[]
-    for particle in 1:se.segments+1
-        push!(x, (sol(sol.t, idxs=pos[1, particle]))[index])
-        push!(z, (sol(sol.t, idxs=pos[3, particle]))[index])
-    end
-    z_max = maximum(z)
-    if isnothing(line)
-        line, = plot(x,z; linewidth="1")
-        sc  = scatter(x, z; s=15, color="red") 
-        txt = annotate("t=$(round(reltime,digits=1)) s",  
-                        xy=(se.l0/4.2, z_max-7), fontsize = 12)
-    else
-        line.set_xdata(x)
-        line.set_ydata(z)
-        sc.set_offsets(hcat(x,z))
-        txt.set_text("t=$(round(reltime,digits=1)) s")
-        gcf().canvas.draw()
+function play(se, sol, pos)
+    dt = 0.151
+    ylim = (-1.2*(se.l0+se.v_ro*se.duration), 0.5)
+    xlim = (-se.l0/2, se.l0/2)
+    mkpath("video")
+    z_max = 0.0
+    # text position
+    xy = (se.l0/4.2, z_max-7)
+    start = time_ns()
+    i = 1; j = 0
+    for time in 0:dt:se.duration
+        # while we run the simulation in steps of 20ms, we update the plot only every 150ms
+        # therefore we have to skip some steps of the result
+        while sol.t[i] < time
+            i += 1
+        end
+        plot2d(sol[pos][i], time; segments=se.segments, xlim, ylim, xy)
+        if se.save
+            ControlPlots.plt.savefig("video/"*"img-"*lpad(j,4,"0"))
+        end
+        j += 1
+        wait_until(start + 0.5*time*1e9)
     end
     if se.save
-        PyPlot.savefig("video/"*"img-"*lpad(j,4,"0"))
-    end
-    line, sc, txt
-end
-
-function play(se, sol, pos)
-    PyPlot.close()
-    dt = 0.151
-    ylim(-1.2*(se.l0+se.v_ro*se.duration), 0.5)
-    xlim(-se.l0/2, se.l0/2)
-    grid(true; color="grey", linestyle="dotted")
-    tight_layout(rect=(0, 0, 0.98, 0.98))
-    line, sc, txt = nothing, nothing, nothing
-    start = time_ns()
-    mkpath("video")
-    for (j, time) in pairs(0:dt:se.duration)
-        line, sc, txt = plot2d(se, sol, pos, time, line, sc, txt, j)
-        wait_until(start + 0.5*time*1e9)
+        include("export_gif.jl")
     end
     nothing
 end
