72bb5c011450d516ec9564da93915fefc7e181f8
Uwe Fechner <fechner@aenarete.eu>
2024-08-17 18:34:45 +0200

    Add runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,30 @@
+using Test, LinearAlgebra
+
+__BENCH__ = true; 
+
+@testset "Tether_06c" begin   
+    # without callbacks
+    include("../src/Tether_06c.jl")
+    set = deepcopy(Settings2())
+    set.duration = 10.0
+    set.callbacks = false
+    simple_sys, pos, vel = model(set)
+    sol, elapsed_time = simulate(set, simple_sys)
+    @test elapsed_time < 1.0
+    l_tether_theoretical = set.l0 + set.v_ro * set.duration
+    @test l_tether(sol, pos) ≈ l_tether_theoretical rtol=2e-3
+    events = Int64(round(length(sol.t)- set.duration/set.dt)-1)
+    @test events == 0
+    
+    # with callbacks
+    set.callbacks = true
+    simple_sys, pos, vel = model(set)
+    sol, elapsed_time = simulate(set, simple_sys)
+    @test elapsed_time < 1.0
+    l_tether_theoretical = set.l0 + set.v_ro * set.duration
+    @test l_tether(sol, pos) ≈ l_tether_theoretical rtol=2e-3
+    events = Int64(round(length(sol.t)- set.duration/set.dt)-1)
+    @test events > 5 # 8 events are expected
+end
+nothing
+
