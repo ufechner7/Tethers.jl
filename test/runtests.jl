415e8247ad5669145cd08cf0eed44ad34452a509
Uwe Fechner <fechner@aenarete.eu>
2024-08-18 01:08:51 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,7 +24,7 @@ __BENCH__ = true;
     l_tether_theoretical = set.l0 + set.v_ro * set.duration
     @test l_tether(sol, pos) ≈ l_tether_theoretical rtol=2e-3
     events = Int64(round(length(sol.t)- set.duration/set.dt)-1)
-    @test events > 5 # 8 events are expected
+    @test events >= 4 # 8 events with Rodas5, 4 events with KenCarp4
 end
 nothing
 
