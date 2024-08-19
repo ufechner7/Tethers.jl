5cdff00c9344838614715ab8a709a14063e101b0
Documenter.jl <documenter@juliadocs.github.io>
2024-08-19 17:36:59 +0000

    build based on 5af9e63

--- dev/src/Tether_07.jl
+++ dev/src/Tether_07.jl
@@ -1,4 +1,4 @@
-# Tutorial example simulating a 3D mass-spring system with a nonlinear spring (1% stiffnes
+# Tutorial example simulating a 3D mass-spring system with a nonlinear spring (1% stiffness
 # for l < l_0), n tether segments, tether drag and reel-in and reel-out. 
 using ModelingToolkit, OrdinaryDiffEq, LinearAlgebra, Timers, Parameters
 using ModelingToolkit: t_nounits as t, D_nounits as D
