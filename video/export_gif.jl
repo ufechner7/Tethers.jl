8350bd42b7726a350fb2bcabe483d16ea6889459
Uwe Fechner <fechner@aenarete.eu>
2024-09-29 12:38:41 +0200

    improve gif generation

--- video/export_gif.jl
+++ video/export_gif.jl
@@ -8,9 +8,11 @@ mp4name = joinpath(folder, "Tether.mp4")
 A = Array{Array{RGB{Normed{UInt8,8}},2},1}()
 files = readdir(folder)
 pngfiles = filter(file->(occursin.("png",file)), files )
+rm(gifname, force=true)
+rm(mp4name, force=true)
 
-FFMPEG.ffmpeg_exe(`-framerate $(framerate) -f image2 -i $(folder)/img-%4d.png -vf palettegen -y $(gifname)`)
 FFMPEG.ffmpeg_exe(`-framerate $(framerate) -f image2 -i $(folder)/img-%4d.png -c:v libx264 -pix_fmt yuv420p -y $(mp4name)`)
+FFMPEG.ffmpeg_exe(`-i $(mp4name) -filter_complex "[0]split[a][b]; [a]palettegen[palette]; [b][palette]paletteuse" -y $(gifname)`)
 
 println("\nGif file $(gifname) created!")
 println("Mp4 file $(mp4name) created!")
