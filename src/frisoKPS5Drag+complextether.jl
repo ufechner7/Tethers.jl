477db0cce605270a39ee81183b57f3ef24458289
Friso Broekhuizen <friso.broekhuizen@hotmail.com>
2025-03-04 15:16:13 +0100

    different masses for bridle,kite and tether

--- src/frisoKPS5Drag+complextether.jl
+++ src/frisoKPS5Drag+complextether.jl
@@ -8,13 +8,13 @@ include("videoKPS5.jl")
 # 🔹 Define struct for simulation settings 
 # 3D: [x,y,z] , x is the heading , z is up and y perpendicular to both
 G_EARTH::Vector{Float64} = [0.0, 0.0, -9.81]
-v_wind_tether::Vector{Float64} = [0.0, 20.0 , 0.0]
-F1::Vector{Float64} = [0.0, 0.0,  20]
-F2::Vector{Float64} = [0.0, 0.0,  25]
-F3::Vector{Float64} = [0.0, 0.0, 27]
-F4::Vector{Float64} = [0.0, 0,  20]
-F5::Vector{Float64} = [0.0,  0, 20]
-tethersegments::Int64 = 2
+v_wind_tether::Vector{Float64} = [0.0, 30 , 0.0]
+F1::Vector{Float64} = [0.0, 0.0,  25]
+F2::Vector{Float64} = [0.0, 0.0,  100]
+F3::Vector{Float64} = [0.0, 0.0, 120]
+F4::Vector{Float64} = [0.0, 0,  50]
+F5::Vector{Float64} = [0.0,  0, 50]
+tethersegments::Int64 = 12
 segments::Int64 = 9 + tethersegments    
 points::Int64 = 5 + tethersegments  
 duration::Float64 = 10.0  # Simulation time [s]
@@ -31,11 +31,11 @@ end
 VEL0 = zeros(3, points)
 
 #add diferent masses for kite, total = 6.2 (KCU 8.4) at bridle
-#tether weight is 
+ 
 
 # defining the model, Z component upwards	
-@parameters K1=4000 K2=500 K3=2000 m=1.0 l1=sqrt(10) l2=2.0 l3=sqrt(10) l4=sqrt(8) l5=sqrt(6) l6=sqrt(6) l7=sqrt(8) l8=sqrt(6) l9=sqrt(6) l10=10 damping=0.9
-@parameters rho=1.225 cd_tether=1.28 d_tether=0.004 
+@parameters K1=614600  K2=10000 K3=60000 m_kite=6.2/4 m_bridle=8.4 rho_tether=724 l1=sqrt(10) l2=2.0 l3=sqrt(10) l4=sqrt(8) l5=sqrt(6) l6=sqrt(6) l7=sqrt(8) l8=sqrt(6) l9=sqrt(6) l10=10 damping=0.9
+@parameters rho=1.225 cd_tether=0.958 d_tether=0.004
 @variables pos(t)[1:3, 1:points]  = POS0
 @variables vel(t)[1:3, 1:points]  = VEL0
 @variables acc(t)[1:3, 1:points]
@@ -79,7 +79,7 @@ for i in 1:segments
        unit_vector[:, i]  ~ -segment[:, i] / norm1[i], 
        rel_vel[:, i]      ~ vel[:, conn[i][2]] - vel[:, conn[i][1]], 
        spring_vel[i]      ~ -unit_vector[:, i] ⋅ rel_vel[:, i],  
-       c_spring[i]        ~ (k_segments[i]/rest_lengths[i]) * (0.25 + 0.75*(norm1[i] > rest_lengths[i])),
+       c_spring[i]        ~ (k_segments[i]/rest_lengths[i]) * (0.1 + 0.9*(norm1[i] > rest_lengths[i])),
        spring_force[:, i] ~ (c_spring[i]*(norm1[i] - rest_lengths[i]) + damping * spring_vel[i]) * unit_vector[:, i],
        v_apparent[:, i]   ~ v_wind_tether .- (vel[:, conn[i][1]] + vel[:, conn[i][2]]) / 2,
        v_app_perp[:, i]   ~ v_apparent[:, i] - (v_apparent[:, i] ⋅ unit_vector[:, i]) .* unit_vector[:, i],
@@ -88,10 +88,12 @@ for i in 1:segments
        #drag_force[:, i]   ~ half_drag_force[:, i]  # assign computed drag to drag_force
     ]
     eqs2 = vcat(eqs2, reduce(vcat, eqs))
-    # (Optional: you can remove the println below so that only numerical values are printed later)
-    # println(half_drag_force[:, i])
 end
-
+#mass distribution
+mass_tether = (d_tether^2 )* pi * rho_tether*l10
+mass_tetherpoints = mass_tether/(tethersegments+1)
+PointMasses = [m_bridle+mass_tetherpoints, m_kite, m_kite, m_kite, m_kite]
+PointMasses = vcat(PointMasses, [mass_tetherpoints for _ in 1:tethersegments]...)
 # Apply force balance for all points
 for i in 1:points  
     global eqs2
@@ -107,7 +109,7 @@ for i in 1:points
     elseif i != 6        # (optional additional constraint)                  
         push!(eqs, total_force[:, i] ~ force)
     end
-    push!(eqs, acc[:, i] ~ G_EARTH + total_force[:, i] / m)
+    push!(eqs, acc[:, i] ~ G_EARTH + total_force[:, i] / PointMasses[i])
     eqs2 = vcat(eqs2, reduce(vcat, eqs))
 end
 
@@ -123,25 +125,31 @@ prob = ODEProblem(simple_sys, nothing, tspan)
 elapsed_time = @elapsed sol = solve(prob, Rodas5(); dt, abstol=tol, reltol=tol, saveat=ts) 
 println("Elapsed time: $(elapsed_time) s, speed: $(round(duration/elapsed_time)) times real-time")
 
-# Extract the total force acting on each point over time
-total_force_sol = sol[total_force, :]
+# # Extract the total force acting on each point over time
+# spring = sol[spring_force, :]
 
-# Print the total force for each point at every saved time step.
-for (i, t_val) in enumerate(ts)
-    println("At t = $(t_val):")
-    for point in 1:points
-        println("  Point $(point) total force: ", total_force_sol[i][:, point])
-    end
-end
-# # Print the drag (drag_force) values for each segment at every saved time step
-# drag_sol = sol[half_drag_force, :]
+# #Print the total force for each point at every saved time step.
 # for (i, t_val) in enumerate(ts)
 #     println("At t = $(t_val):")
-#     for seg in 1:segments
-#          # Extract the 3D drag vector for this segment at this time step
-#          println("  Segment $(seg) drag: ", drag_sol[i][:, seg])
+#     for point in 1:points
+#         println("  Point $(point) spring force: ", norm(spring[i][:, point]))
 #     end
 # end
+# # # Print the drag (drag_force) values for each segment at every saved time step
+# # drag_sol = sol[half_drag_force, :]
+# # for (i, t_val) in enumerate(ts)
+# #     println("At t = $(t_val):")
+# #     for seg in 1:segments
+# #          # Extract the 3D drag vector for this segment at this time step
+# #          println("  Segment $(seg) drag: ", drag_sol[i][:, seg])
+# #     end
+# # end
+
+# ground_force_spring=(sol[spring_force,10])
+# ground_force_drag=(sol[half_drag_force,10])
+# ground_force = ground_force_spring + ground_force_drag
+# println("Ground force: ", norm.(eachcol(ground_force)))
+
 
 # Plotting
 # Extract solution positions
