52b45dbd00269c524da9ef46581288e1c86b34bb
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-03-14 20:48:58 +0100

    Some progress

--- examples_quasistatic/benchmark_qsm.jl
+++ examples_quasistatic/benchmark_qsm.jl
@@ -5,7 +5,10 @@ include("../src/Tether_quasistatic.jl")
 # Read the initial conditions from a .mat file
 state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = get_initial_conditions("test/data/input_basic_test.mat")
 
+buffers = init(state_vec, wind_vel)
 simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings; prn=true)
+# simulate_tether!(buffers, state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings; prn=true)
+
 @benchmark simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
 
 #= julia> include("examples_quasistatic/benchmark_qsm.jl")
