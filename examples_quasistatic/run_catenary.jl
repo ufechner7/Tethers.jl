a9e91ca2867bd3f29d0d8dc4aa024bb68ded1946
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-03-14 19:35:46 +0100

    Improve comments

--- examples_quasistatic/run_catenary.jl
+++ examples_quasistatic/run_catenary.jl
@@ -1,13 +1,13 @@
 include("../src/Tether_quasistatic.jl")
 
-# Get information from the .mat
+# Read the initial conditions from the .mat
 state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = get_initial_conditions("test/data/input_basic_test.mat")
 state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
 
 x_qs = vec(sqrt.(tether_pos[1,:].^2 + tether_pos[2,:].^2))
 y_qs = vec(tether_pos[3,:])
 
-# Get catenary curve
+# Read the catenary curve
 x_cat, y_cat = get_analytic_catenary("test/data/input_analytic_catenary.mat")
 
 plt.plot(x_cat, y_cat)
