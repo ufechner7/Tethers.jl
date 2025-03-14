31fda30a19a5eae2aeea6ed93365b78de4d9850b
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-03-14 20:08:07 +0100

    Fix example

--- examples_quasistatic/force_plots.jl
+++ examples_quasistatic/force_plots.jl
@@ -2,17 +2,11 @@ using StaticArrays, LinearAlgebra
 include("../src/Tether_quasistatic.jl")
 
 function main()
+    # Read the initial conditions from a .mat file
+    state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = get_initial_conditions("test/data/input_basic_test.mat")
 
-# Get information from the .mat
-    state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = getInitCond("test/data/input_basic_test.mat")
-    Fobj_ref, p0_ref, pj_ref, T0_ref = getOutputObjFun("test/data/basic_test_results.mat")
-
-
-
-
-kite_pos = MVector(5, 100, 300)
-state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  tetherQuasiStatic(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
-
+    kite_pos = MVector(5, 100, 300)
+    state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
 
     plt.figure("3D view").add_subplot(projection="3d")
     plt.plot3D(tether_pos[1,:], tether_pos[2,:], tether_pos[3,:], marker = "o")
@@ -20,14 +14,11 @@ state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  tetherQuasiStatic(state_vec, ki
     plt.scatter3D(p0[1], p0[2], p0[3], s = 50, marker = "D", c = "g")
     plt.legend(["Tether", "Origin", "Kite"])
 
-
     plt.figure("2D view").add_subplot()
     plt.plot(sqrt.(tether_pos[1,:].^2 + tether_pos[2,:].^2), (tether_pos[3,:]), marker = "o")
     plt.scatter(0, 0, s = 200, marker = "s", c = "C7")
     plt.scatter(sqrt(p0[1]^2 +  p0[2]^2), p0[3], s = 50, marker = "D", c = "g")
     
-
-
     x_positions = LinRange(100, 300, 6)
 
     ax1 = plt.figure("2D view for different kite positions").add_subplot()
@@ -35,7 +26,7 @@ state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  tetherQuasiStatic(state_vec, ki
 
     for ii = 1:length(x_positions)
         kite_pos = MVector(5, x_positions[ii], 300)
-        state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  tetherQuasiStatic(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
+        state_vec, tether_pos, Ft_ground, Ft_kite, p0 = simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
         
         ax1.plot(sqrt.(tether_pos[1,:].^2 + tether_pos[2,:].^2), (tether_pos[3,:]), marker = "o", c = "C0")
         ax1.scatter(0, 0, s = 200, marker = "s", c = "C7")
@@ -43,12 +34,10 @@ state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  tetherQuasiStatic(state_vec, ki
 
         distance = sqrt(p0[1]^2 + p0[2]^2 + p0[3]^2)
         ax2.scatter(distance, Ft_ground, c = "C0", s=20,  alpha=0.5)
-        ax2.scatter(distance, norm(Ft_kite), c = "C1", s=20, alpha=0.5)
-        
+        ax2.scatter(distance, norm(Ft_kite), c = "C1", s=20, alpha=0.5)        
     end
     ax1.legend(["Tether", "Ground station"])
     ax2.legend(["Tension at ground station", "Tension at kite"])
-
     plt.show()
 end
 
