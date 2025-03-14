6ebf2d2b01745b912e039bf15ece5af479f18b7b
Uwe Fechner <fechner@aenarete.eu>
2025-03-15 00:50:34 +0100

    Cleanup

--- examples_quasistatic/benchmark_qsm.jl
+++ examples_quasistatic/benchmark_qsm.jl
@@ -4,23 +4,23 @@ include("../src/Tether_quasistatic.jl")
 
 # Read the initial conditions from a .mat file
 state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = get_initial_conditions("test/data/input_basic_test.mat")
-# state_vec, tether_pos, force_gnd, force_kite, p0 = simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
 simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings; prn=true)
 
 @benchmark simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
 
-#= julia> include("examples_quasistatic/benchmark_qsm.jl")
+#= On Ryzen 7950X 
+julia> include("examples_quasistatic/benchmark_qsm.jl")
+Iterations: 36
 BenchmarkTools.Trial: 10000 samples with 1 evaluation per sample.
- Range (min … max):  113.274 μs …   9.631 ms  ┊ GC (min … max): 0.00% … 96.95%
- Time  (median):     116.019 μs               ┊ GC (median):    0.00%
- Time  (mean ± σ):   119.865 μs ± 141.114 μs  ┊ GC (mean ± σ):  2.24% ±  2.13%
+ Range (min … max):   93.927 μs …   8.020 ms  ┊ GC (min … max): 0.00% … 98.07%
+ Time  (median):     102.166 μs               ┊ GC (median):    0.00%
+ Time  (mean ± σ):   106.874 μs ± 117.790 μs  ┊ GC (mean ± σ):  2.18% ±  2.32%
 
-       ▄█▇▄▁                                                     
-  ▁▁▂▄██████▅▄▃▃▃▃▃▂▂▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▂
-  113 μs           Histogram: frequency by time          135 μs <
+     █▇▃▁                                                        
+  ▂▃█████▇▇▆▇▆▆▆▆▆▅▅▅▅▅▅▅▅▅▅▅▅▄▅▄▄▄▄▄▄▃▃▃▃▃▃▃▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▁▂ ▄
+  93.9 μs          Histogram: frequency by time          133 μs <
 
- Memory estimate: 46.34 KiB, allocs estimate: 1116. 
- On Uwes Laptop on battery, fast mode.
+ Memory estimate: 46.53 KiB, allocs estimate: 1118. 
  =#
 
 
