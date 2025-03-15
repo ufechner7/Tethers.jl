0a0d985238f56ceb7316a0c1acf4af24313a9772
Uwe Fechner <fechner@aenarete.eu>
2025-03-15 14:23:12 +0100

    Fix tests

--- test/test_basic.jl
+++ test/test_basic.jl
@@ -1,8 +1,12 @@
 using Test
 include("../src/Tether_quasistatic.jl")
 
-@testset "ObjFun_test" begin  
-    state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = getInitCond("test/data/input_basic_test.mat")
+if basename(pwd())=="test"
+    cd("..")
+end
+
+@testset "residual_test" begin  
+    state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = get_initial_conditions("test/data/input_basic_test.mat")
     # Set up other parameters
     Ns = size(wind_vel, 2)
     buffers= [zeros(3, Ns), zeros(3, Ns), zeros(3, Ns), zeros(3, Ns), zeros(3, Ns)]
@@ -10,7 +14,7 @@ include("../src/Tether_quasistatic.jl")
     # Pack in param tuple
     param = (kite_pos, kite_vel, wind_vel, tether_length, settings, buffers, true)
     # Call objective function
-    Fobj, T0, pj, p0 = objFun!(res, state_vec, param)
+    Fobj, T0, pj, p0 = res!(res, state_vec, param)
 
     # Test type
     @test Fobj isa Vector
@@ -19,7 +23,7 @@ include("../src/Tether_quasistatic.jl")
     @test pj isa Matrix
 
     # Get reference values from .mat
-    Fobj_ref, p0_ref, pj_ref, T0_ref = getOutputObjFun("test/data/basic_test_results.mat")
+    Fobj_ref, p0_ref, pj_ref, T0_ref = get_test_output("test/data/basic_test_results.mat")
     # Test values
     @test Fobj == Fobj_ref
     @test T0 == T0_ref
