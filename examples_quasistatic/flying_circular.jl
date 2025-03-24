47ea1f6f8e5d639db07dad7c82ab98b225f807ef
AndreaBertozzi <a.bertozzi@student.tudelft.nl>
2025-03-24 14:00:32 +0100

    New working example

--- examples_quasistatic/flying_circular.jl
+++ examples_quasistatic/flying_circular.jl
@@ -1,4 +1,6 @@
+using LaTeXStrings
 include("../src/Tether_quasistatic.jl")
+
 
 function main()
     avg_el = deg2rad(70)
@@ -16,37 +18,19 @@ function main()
     for ii = 1:size(traj)[2]
     traj[:,ii] .= rot_mat*traj[:,ii]
     end
-
-
-    fig = plt.figure("3D view")
-    ax = fig.add_subplot(111, projection="3d")
-
-    # Scatter plot of the trajectory
-    ax.scatter3D(traj[1, :], traj[2, :], traj[3, :])
-
-    # Plot a line from origin to the last point
-    #ax.plot3D([0, traj[1, end]], [0, traj[2, end]], [0, traj[3, end]])
-    ax.plot3D([0, 0], [0, -traj_dist*sin(avg_el)], [0, traj_dist*cos(avg_el)], ":")
-    plt.xlabel("X [m]")
-    plt.ylabel("Y [m]")
-    plt.zlabel("Z [m]")
-    plt.xlim((-250, 250))
-    plt.zlim((-50, 450))
-    plt.title("Circular trajectory")
-    plt.legend(["Kite trajectory", "Average elevation axis"])
-    # Show the plot
-    plt.show()
-    #traj = rot_mat*traj
-
+    
     # Initial position gamma = 0
     kite_pos = MVector{3}(traj[:, 1])
     # Initialise model
-    tether_length = 400
+    tether_length = 300
     segments = 10
     state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = init_quasistatic(kite_pos, tether_length, segments = segments)
+    tether_length = 400
     state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
-
+    tether_length = 500
+    state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
     tether_pos = hcat(p0, tether_pos, [0; 0; 0])
+
     plt.figure("3D view").add_subplot(projection="3d").set_aspect("equal")
     plt.plot3D(tether_pos[1,:], tether_pos[2,:], tether_pos[3,:], marker = "o")
     plt.scatter3D(0, 0, 0,  s = 200, marker = "s", c = "C7")
@@ -56,13 +40,39 @@ function main()
     plt.legend(["Tether", "Origin", "Kite"])
     plt.show()
 
-    all_tether_pos = zeros(length(gamma), 3, segments)
-    Ft_ground = zeros(length(gamma), 1)
+    all_tether_pos = zeros(length(gamma), 3, segments + 1)
+    all_Ft_kite = zeros(3, length(gamma))
+    all_Ft_ground = zeros(length(gamma))
     
     for ii = 1:length(gamma)
-        state_vec, all_tether_pos[ii, :, :], Ft_ground[ii, :], Ft_kite, p0 = simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
+        kite_pos .= MVector{3}(traj[:, ii])
+        state_vec, tether_pos, Ft_ground, Ft_kite, p0 = simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
+        state_vec .= state_vec
+        tether_pos = hcat(p0, tether_pos)
+        all_tether_pos[ii, :, :] .= tether_pos   
+        all_Ft_kite[:, ii] .= Ft_kite
+        all_Ft_ground .= Ft_ground
     end
-
+    
+    plt.figure("2D plot")
+    plt.plot(gamma, all_Ft_kite[1, :]./1000)
+    plt.plot(gamma, all_Ft_kite[2, :]./1000)
+    plt.plot(gamma, all_Ft_kite[3, :]./1000)
+    plt.legend([L"F_x", L"F_y", L"F_z"])    
+    plt.xlabel(L"\gamma [rad]")
+    plt.ylabel("Force [kN]")
+    plt.title("Tether force components at kite during a circular trajectory")
+    plt.show()
     
 
+    plt.figure("3D view").add_subplot(projection="3d").set_aspect("equal")
+    plt.scatter3D(0, 0, 0,  s = 200, marker = "s", c = "C7")
+    plt.scatter3D(traj[1, :], traj[2, :], traj[3, :])
+    for ii = 1:length(gamma)
+        plt.plot3D(all_tether_pos[ii,1,:], all_tether_pos[ii,2,:], all_tether_pos[ii,3,:], marker = "x", color = "C1", linestyle = ":")
+    end
+    plt.xlabel("X [m]")
+    plt.ylabel("Y [m]")
+    plt.legend(["Origin", "Kite trajectory", "Tethers"])
+    plt.show()   
 end
