e61ca68f4f172595f2cd2118e7cd5484beae4c98
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-19 12:37:04 +0100

    Re-enable all includes

--- test/test_for_precompile.jl
+++ test/test_for_precompile.jl
@@ -2,10 +2,10 @@ __PC=true
 let 
     include("../src/init.jl")
     include("../src/Tether_01.jl")
-    # include("../src/Tether_02.jl")
-    # include("../src/Tether_03.jl")
-    # include("../src/Tether_08.jl")
-    # GC.gc()
+    include("../src/Tether_02.jl")
+    include("../src/Tether_03.jl")
+    include("../src/Tether_08.jl")
+    GC.gc()
     nothing   
 end
 
