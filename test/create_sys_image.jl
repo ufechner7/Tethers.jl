ce47fc43572b751498ad52821d6a75fafc2fd836
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-26 03:30:07 +0100

    Improve create_sys_image

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -11,6 +11,16 @@ if FAST
     push!(pkgs, :ControlPlots)
 end 
 
+GC.gc(true)
+let mem = Sys.free_memory() / 1024^2
+    @info "Free memory: $(round(mem; digits=1)) MB"
+    if haskey(ENV, "JULIA_IMAGE_THREADS")
+        @info "JULIA_IMAGE_THREADS: $(ENV["JULIA_IMAGE_THREADS"])"
+    else
+        @info "JULIA_IMAGE_THREADS not defined!"
+    end
+end
+
 PackageCompiler.create_sysimage(
     pkgs;
     sysimage_path="kps-image_tmp.so",
--- test/test_for_precompile.jl
+++ test/test_for_precompile.jl
@@ -5,7 +5,16 @@ let
     include("../src/Tether_02.jl")
     include("../src/Tether_03.jl")
     include("../src/Tether_08.jl")
-    GC.gc()
+    
+    GC.gc(true)
+    let mem = Sys.free_memory() / 1024^2
+        @info "Free memory: $(round(mem; digits=1)) MB"
+        if haskey(ENV, "JULIA_IMAGE_THREADS")
+            @info "JULIA_IMAGE_THREADS: $(ENV["JULIA_IMAGE_THREADS"])"
+        else
+            @info "JULIA_IMAGE_THREADS not defined!"
+        end
+    end
     nothing   
 end
 
