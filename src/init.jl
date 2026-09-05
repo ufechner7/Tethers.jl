2a25c6fc9f8485e7dab986c69d3b869b5ca8281a
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-09-05 12:05:45 +0200

    Better menu

--- src/init.jl
+++ src/init.jl
@@ -3,20 +3,23 @@ println("Running init...")
 using MakieControlPlots
 using REPL.TerminalMenus
 
-options = ["include(\"src/Tether_01.jl\")",
-           "include(\"src/Tether_02.jl\")",
-           "include(\"src/Tether_03.jl\")",
-           "include(\"src/Tether_03b.jl\")",
-           "include(\"src/Tether_03c.jl\")",
-           "include(\"src/Tether_04.jl\")",
-           "include(\"src/Tether_05.jl\")",
-           "include(\"src/Tether_06.jl\")",
-           "include(\"src/Tether_06b.jl\")",
-           "include(\"src/Tether_06c.jl\")",
-           "include(\"src/Tether_07.jl\")",
-           "include(\"src/Tether_08.jl\")",
-           "include(\"src/Tether_09.jl\")",
-           "quit()"]
+examples = [("Tether_01",  "include(\"src/Tether_01.jl\")",  "Falling mass thrown upwards"),
+            ("Tether_02",  "include(\"src/Tether_02.jl\")",  "Mass on a linear spring-damper"),
+            ("Tether_03",  "include(\"src/Tether_03.jl\")",  "Mass on a non-linear spring-damper"),
+            ("Tether_03b", "include(\"src/Tether_03b.jl\")", "Non-linear spring with callback"),
+            ("Tether_03c", "include(\"src/Tether_03c.jl\")", "Benchmark: callback vs no callback"),
+            ("Tether_04",  "include(\"src/Tether_04.jl\")",  "Multi-segment tether (2D arrays)"),
+            ("Tether_05",  "include(\"src/Tether_05.jl\")",  "Segmented tether, correct force split"),
+            ("Tether_06",  "include(\"src/Tether_06.jl\")",  "Segmented tether, reeling out"),
+            ("Tether_06b", "include(\"src/Tether_06b.jl\")", "Reel-out tether, refactored with Settings"),
+            ("Tether_06c", "include(\"src/Tether_06c.jl\")", "Reel-out tether with continuous callback"),
+            ("Tether_07",  "include(\"src/Tether_07.jl\")",  "Segmented tether with aerodynamic drag"),
+            ("Tether_08",  "include(\"src/Tether_08.jl\")",  "Tether with arbitrary/free endpoints"),
+            ("Tether_09",  "include(\"src/Tether_09.jl\")",  "Labeled tether shape diagram for docs")]
+
+name_width = maximum(length(name) for (name, _, _) in examples)
+options = [rpad(name, name_width) * "  " * descr for (name, _, descr) in examples]
+push!(options, "quit()")
 
 function menu()
     active = true
@@ -25,7 +28,7 @@ function menu()
         choice = request("\nChoose function to execute or `q` to quit: ", menu)
 
         if choice != -1 && choice != length(options)
-            eval(Meta.parse(options[choice]))
+            eval(Meta.parse(examples[choice][2]))
         else
             println("Left menu. Press <ctrl><d> to quit Julia!")
             active = false
